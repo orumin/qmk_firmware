3364b6367e3569b4c4f34bb8cb4abecbf1493d8f
itou <hsginjake123@gmail.com>
2019-08-20 17:58:55 +0900

    pov

--- keyboards/lovelive_blade11/keymaps/default/keymap.c
+++ keyboards/lovelive_blade11/keymaps/default/keymap.c
@@ -43,6 +43,8 @@ enum Layer
   _MARI,
   _KANAN,
   _LOCK,
+  _TO_POV,
+  _POV,
 };
 
 
@@ -61,11 +63,35 @@ enum custom_keycodes {
   RIKO,
   MARI,
   KANAN,
-  TO_SCHOOL_IDOL_FESTIVAL,
-  RETURN_SCHOOL_IDOL_FESTIVAL
+  TO_POV1,
+  TO_POV2,
+  POV_INC,
+  POV_DEC,
+  POV_END,
 };
 int aqours_index = 0;
 int aqours_led_anim_index = 0;
+
+int pov_mode = 0;
+#define ELECTRIC_BOARD_LENGTH 52
+int electric_board_data[5][ELECTRIC_BOARD_LENGTH] = {
+  {0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0},
+  {1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0},
+  {0,0,1,0,0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0,1,1,0,0,0,0,0,0},
+  {1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,1,1,0,0,0},
+  {0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0},
+};
+/*
+  ■                    ■    ■               ■   ■
+■■■■■               ■■■■■■■  ■              ■     ■
+  ■      ■■■■■■        ■         ■■■■■■     ■■
+■■■■■               ■  ■  ■                 ■  ■■
+  ■                ■   ■   ■                ■
+  */
+int read_num = 0;
+int pov_switch_time = 100;
+int pov_switch_count =0;
+
 #define _______ KC_TRNS
 
 int long_tap_timer;
@@ -75,7 +101,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     TIKA, RIKO, KANAN, DIA, YOU, YOHANE, ZURA, MARI, RUBY, RGB_TOG, TG(_LOCK)
   ),
   [_LOCK] = LAYOUT(
-    KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, RGB_TOG, KC_TRNS
+    KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, TO_POV1, KC_9, RGB_TOG, KC_TRNS
+  ),
+  [_TO_POV] = LAYOUT(
+    KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, TO_POV1, TO_POV2, RGB_TOG, KC_TRNS
+  ),
+  [_POV] = LAYOUT(
+    POV_INC, POV_DEC, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, POV_END, POV_END
   ),
 };
 
@@ -178,10 +210,64 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
       break;
-  }
 
+  case TO_POV1:
+    if (record->event.pressed) {
+      layer_on(_TO_POV);
+    } else {
+      layer_off(_TO_POV);
+    }
+    return false;
+    break;
+
+  case TO_POV2:
+    if (record->event.pressed) {
+      pov_mode = true;
+      layer_on(_POV);
+    }
+    return false;
+    break;
+
+  case POV_END:
+    if (record->event.pressed) {
+      pov_mode = false;
+      layer_off(_POV);
+    }
+    return false;
+    break;
+
+  case POV_INC:
+    if (record->event.pressed) {
+        pov_switch_time+=10;
+    }
+    return false;
+    break;
+
+  case POV_DEC:
+    if (record->event.pressed) {
+      pov_switch_time+= -10;
+    }
+    return false;
+    break;
+  }
   return true;
 }
+
+int aqours_led_timer_count = 0;
+int aqours_led_auto_timer_count = 0;
+void aqours_led(void) {
+  aqours_led_timer_count++;
+  if (aqours_led_timer_count > 500) {
+    aqours_led_timer_count = 0;
+    if (aqours_led_anim_index < 6) {
+      sethsv(aqours_color_h[aqours_index], aqours_color_s[aqours_index], aqours_color_v[aqours_index]-30, (LED_TYPE *)&led[aqours_led_anim_index]);
+      sethsv(aqours_color_h[aqours_index], aqours_color_s[aqours_index], aqours_color_v[aqours_index]-30, (LED_TYPE *)&led[11-aqours_led_anim_index]);
+      aqours_led_anim_index++;
+      rgblight_set();
+    }
+  }
+}
+
 void matrix_init_user(void) {
   LED_layer_set(3);
   //qk_ucis_start();
@@ -200,25 +286,47 @@ void matrix_init_user(void) {
   rgblight_set();
   */
 }
-int aqours_led_timer_count = 0;
-int aqours_led_auto_timer_count = 0;
-void aqours_led(void) {
-  aqours_led_timer_count++;
-  if (aqours_led_timer_count > 500) {
-    aqours_led_timer_count = 0;
-    if (aqours_led_anim_index < 6) {
-      sethsv(aqours_color_h[aqours_index], aqours_color_s[aqours_index], aqours_color_v[aqours_index]-30, (LED_TYPE *)&led[aqours_led_anim_index]);
-      sethsv(aqours_color_h[aqours_index], aqours_color_s[aqours_index], aqours_color_v[aqours_index]-30, (LED_TYPE *)&led[11-aqours_led_anim_index]);
-      aqours_led_anim_index++;
-      rgblight_set();
+
+
+void pov_lighting(void) {
+  pov_switch_count++;
+  if (pov_switch_count > pov_switch_time) {
+    pov_switch_count = 0;
+    int pov_hue = 0;
+    if (read_num < 5) {
+       pov_hue = 0;
+    } else if (read_num < 14) {
+       pov_hue = 80;
+    } else if (read_num < 29) {
+       pov_hue = 150;
+    } else if (read_num < 38) {
+       pov_hue = 220;
+    } else {
+       pov_hue = 280;
     }
+
+    read_num++;
+    if (read_num > ELECTRIC_BOARD_LENGTH)  {
+        read_num = read_num - ELECTRIC_BOARD_LENGTH;
+    }
+
+
+
+
+    for (int c=0;c<5;c++) {
+        sethsv(pov_hue, 100, 50*electric_board_data[c][read_num], (LED_TYPE *)&led[4-c]);
+        sethsv(pov_hue, 100, 50*electric_board_data[c][read_num], (LED_TYPE *)&led[c+5]);
+    }
+    sethsv(100, 100, 0, (LED_TYPE *)&led[10]);
+    sethsv(100, 100, 0, (LED_TYPE *)&led[11]);
+    rgblight_set();
   }
 }
 
-
 void matrix_scan_user(void) {
 
-
+    if (pov_mode) {
+      pov_lighting();
+    }
     aqours_led();
-
 }
