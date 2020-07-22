ea2320a5aecbad4eccb625c03cee894e3bb2344d
hsginjake <hsginjake123@gmail.com>
2020-07-23 02:11:59 +0900

    多分動くと思うんだけど、どうだろう？

--- keyboards/ec_helix/rules.mk
+++ keyboards/ec_helix/rules.mk
@@ -27,8 +27,8 @@ SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 # if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 NKRO_ENABLE = no            # USB Nkey Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight.
-LED_ANIMATIONS = no        # LED animations
+RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight.
+LED_ANIMATIONS = yes        # LED animations
 MIDI_ENABLE = no            # MIDI support
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
